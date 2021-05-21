3f44a67280357f9376cb0d14794ea6497fbe6468
Antonio Gonzalez <goantonio31@outlook.com>
2021-05-20 22:03:30 -0700

    finished the banner and the nav components. Giving the na a background scroll effect and installed the youtube-react, movie-trailer packages

--- netflix-clone/src/components/Nav.js
+++ netflix-clone/src/components/Nav.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/nav.css';
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+    // This allows the navbar to have a scroll effect after scrolling down 45px the nav will use the scroll effect
+    useEffect(() => {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 450 ) {
+            handleShow(true);
+        } else handleShow(false);
+    });
+    return () => {
+        window.removeEventListener("scroll");
+    };
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav_black"}`}>
             <img 
                 className="nav_logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
