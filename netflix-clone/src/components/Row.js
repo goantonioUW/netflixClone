44298ee1a5d3bdacc891fb4362f93c9a01803f3c
Antonio Gonzalez <goantonio31@outlook.com>
2021-05-21 17:29:27 -0700

    Added a  handleClick function to get a youtube trailer of the moive clicked. Tre is a bug that needs to be fixed some videos will not play trailers.

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from "react";
 import axios from '../axios';
 import '../css/row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchURL, isLargeRow}) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     //A snippet of code which runs based on specific condition/variable
     useEffect(() => {
@@ -16,32 +19,53 @@ function Row({title, fetchURL, isLargeRow}) {
             setMovies(request.data.results);
             return request;
         }
-        fetchData()
-    }, [fetchURL])
+        fetchData();
+    }, [fetchURL]);
 
-    // console.log(movies)
+    const opts = {
+        height: "400",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                // https://www.youtube.com/watch?v=CYurQUQwNtk
+                const urlParams = new URLSearchParams(new URL(url).search);
+                //This sets the TrailerUrl to the video id stored in the param as v=CYurQUQwNtk
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error) => console.log(error));
+        }
+    }
+    console.log(movies)
 
     return (
         <div className="row">
-            {/* TITLE */}
             <h2>{title}</h2>
 
             <div className="row_posters">
-            {/* several row posers */}
-
-            {movies.map(movie => (
-                <img 
-                key={movie.id}
-                className={`row_poster ${isLargeRow && "row_postserLarge"}`}
-                src={`${base_url}${
-                    isLargeRow ? movie.poster_path : movie.backdrop_path}
-                    `} 
-                alt={movie.name} />
-            ))}
+                {movies.map(movie => (
+                    <img 
+                    key={movie.id}
+                    onClick={() => handleClick(movie)}
+                    className={`row_poster ${isLargeRow && "row_postserLarge"}`}
+                    src={`${base_url}${
+                        isLargeRow ? movie.poster_path : movie.backdrop_path
+                    }`} 
+                    alt={movie.name} 
+                    />
+                ))}
             </div>
-          
-        </div>
-    )
+       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+       </div>
+    );
 };
 
 export default Row
